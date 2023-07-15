af67adc23136490068384df763d33969cff031b6
Tran Khoa <khoatranpc603@gmail.com>
2023-07-16 01:03:29 +0700

    update logic

--- src/components/ManagerClass/Detail/OverView/index.tsx
+++ src/components/ManagerClass/Detail/OverView/index.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useLayoutEffect, useMemo } from 'react';
 import { useRouter } from 'next/router';
 import { Popover } from 'antd';
+import { InfoCircleOutlined } from '@ant-design/icons';
 import { Obj } from '@/global/interface';
-import { KEY_ICON, ROLE_TEACHER, STATUS_CLASS } from '@/global/enum';
+import { KEY_ICON, ROLE_TEACHER, STATUS_CLASS, Weekday } from '@/global/enum';
 import { MapIconKey } from '@/global/icon';
-import { getColorFromStatusClass, mapStatusToString } from '@/global/init';
+import { getColorFromStatusClass, getOrderWeekday, mapStatusToString } from '@/global/init';
 import { formatDatetoString, uuid } from '@/utils';
 import { useDetailClass, useQueryBookTeacher } from '@/utils/hooks';
 import BlockNotifi from './BlockNotifi';
@@ -27,37 +28,58 @@ const OverView = () => {
             bookTeacherRQ.query!(router.query.classId as string);
         }
     }, []);
-    console.log(detailClass);
+    // logic call api not equal current data in reducer with router
+    useLayoutEffect(() => {
+        const crrStoreDetailClass = detailClass.data.response?.data._id as string;
+        const crrClassId = router.query.classId as string;
+        if (crrStoreDetailClass !== crrClassId) {
+            detailClass.query!(crrClassId);
+            bookTeacherRQ.query!(router.query.classId as string);
+        }
+    }, []);
     const dataOverView: Array<ItemOverView> = useMemo(() => {
-        const dtSt: { teacherId: string; fullName: string; }[] = [];
+        const dtSt: { teacherId: string; fullName: string; groupNumber: number }[] = [];
         const listMT: Obj[] = [];
-        const dtMt: { teacherId: string; fullName: string; }[] = [];
+        const dtMt: {
+            teacherId: string;
+            fullName: string;
+            locationId: string;
+            group: number;
+            roleRegister: ROLE_TEACHER
+        }[] = [];
 
         const locationDt: Obj[] = [];
-
-        // pending logic filter mentor with accept=true
+        const dataDetailClass = detailClass.data.response as Obj;
         const ST = (((bookTeacherRQ.data as Obj)?.response as Obj)?.data as Array<Obj>)?.filter((item) => {
             const getTeacherRegister = item.teacherRegister as Array<Obj>;
             const crrST = getTeacherRegister.find((req) => {
                 return req.accept === true && (req.roleRegister as ROLE_TEACHER) === ROLE_TEACHER.ST
             });
-            // console.log(item);
             locationDt.push({
                 ...item.locationId,
                 groupNumber: item.groupNumber,
-                tc: ''
             });
             if (!crrST) {
-                listMT.push(item);
+                const teacherMtSp = getTeacherRegister.find((req) => {
+                    return req.accept === true && ((req.roleRegister as ROLE_TEACHER) === ROLE_TEACHER.SP || (req.roleRegister as ROLE_TEACHER) === ROLE_TEACHER.MT)
+                })
+                if (teacherMtSp) {
+                    listMT.push({
+                        ...teacherMtSp,
+                        location: item.locationId._id,
+                        groupNumber: item.groupNumber
+                    });
+                }
             }
             return crrST;
         }) || [];
         listMT.forEach((item) => {
-            (item.teacherRegister as Array<Obj>).map((tc) => {
-                dtMt.push({
-                    teacherId: tc.idTeacher._id as string,
-                    fullName: tc.idTeacher.fullName as string
-                })
+            dtMt.push({
+                teacherId: item.idTeacher._id as string,
+                fullName: item.idTeacher.fullName as string,
+                locationId: item.location as string,
+                group: item.groupNumber as number,
+                roleRegister: item.roleRegister as ROLE_TEACHER
             })
         });
 
@@ -65,7 +87,8 @@ const OverView = () => {
             (item.teacherRegister as Array<Obj>).forEach((tc) => {
                 dtSt.push({
                     teacherId: tc.idTeacher._id as string,
-                    fullName: tc.idTeacher.fullName as string
+                    fullName: tc.idTeacher.fullName as string,
+                    groupNumber: item.groupNumber as number
                 })
             });
         });
@@ -102,42 +125,69 @@ const OverView = () => {
                 }
             ]
         };
-
         // hành chính
         const dataADM = {
             title: 'Hành chính',
             data: [
                 {
                     title: 'Ngày khai giảng',
-                    value: [formatDatetoString(new Date(((detailClass.data.response as Obj)?.data as Obj)?.dayRange.start as string))]
+                    value: [formatDatetoString((((detailClass.data.response as Obj)?.data as Obj)?.dayRange?.start as string) || new Date(), 'dd/MM/yyyy')]
                 },
                 {
                     title: 'Cơ sở',
-                    value: locationDt.map((item) => {
-                        console.log(item);
-                        return <span>
+                    value: locationDt.map((item, index) => {
+                        const tcInLocation = dtMt.filter((tc) => {
+                            return tc.locationId === item._id && tc.group === item.groupNumber;
+                        });
+                        const stInLocation = getSt.filter((record) => {
+                            return record.groupNumber === item.groupNumber;
+                        })
+                        return <span key={index}>
                             {`Nhóm ${item.groupNumber} - ${item.locationCode as string}`}
                             <Popover
-                                content={<span>
-                                    GV|MT{ }
-                                </span>}
+                                content={
+                                    <div>
+                                        {tcInLocation.map((record, idx) => {
+                                            return <span key={idx}>
+                                                {record.roleRegister}-{record.fullName}
+                                            </span>
+                                        })
+                                        }
+                                        {
+                                            stInLocation.map((record, idx) => {
+                                                return <span key={idx}>
+                                                    {ROLE_TEACHER.ST}-{record.fullName}
+                                                </span>
+                                            })
+                                        }
+                                        {
+                                            tcInLocation.length === 0 && stInLocation.length == 0 && <span><u>Ôi, chưa có GV nào hết!</u></span>
+                                        }
+                                    </div>
+                                }
                             >
-                                <sup className={styles.supI}>i</sup>
+                                <sup className={styles.supI} style={{ cursor: 'pointer', width: '5px', fontSize: '-0.25em', marginLeft: '2px' }}>
+                                    <InfoCircleOutlined style={{ color: '#BB0409' }} />
+                                </sup>
                             </Popover>
                         </span>
                     })
                 },
                 {
                     title: 'Trạng thái',
-                    value: [<span className="display-block status" key={uuid()} style={{ backgroundColor: getColorFromStatusClass[STATUS_CLASS.RUNNING] }}>{mapStatusToString[STATUS_CLASS.RUNNING]}</span>]
+                    value: [<span className="display-block status" key={uuid()} style={{ backgroundColor: getColorFromStatusClass[dataDetailClass?.data?.status as STATUS_CLASS || STATUS_CLASS.PREOPEN] }}>{mapStatusToString[dataDetailClass?.data?.status as STATUS_CLASS || STATUS_CLASS.PREOPEN]}</span>]
                 },
                 {
                     title: 'Lịch học',
-                    value: ['T2: 19h15-22h15', 'T6: 14h-17h']
+                    value: [...(dataDetailClass?.data.timeSchedule as Array<Obj>) || []]?.sort((a, b) => {
+                        return getOrderWeekday[a.weekday as Weekday] - getOrderWeekday[b.weekday as Weekday] || 0;
+                    }).map((item) => {
+                        return `${item.weekday}: ${String(item.start).replace(/\s/g, '').slice(0, String(item.start).length - 3)}-${String(item.end).replace(/\s/g, '').slice(0, String(item.end).length - 3)}`
+                    })
                 },
                 {
                     title: 'Khoá',
-                    value: ['PMX']
+                    value: [`${dataDetailClass?.data.courseId.courseName}-${dataDetailClass?.data.courseLevelId.levelCode}`]
                 },
                 {
                     title: 'Địa điểm',
@@ -179,7 +229,7 @@ const OverView = () => {
             }
         ];
         return dataOverView;
-    }, [bookTeacherRQ.data]);
+    }, [bookTeacherRQ, detailClass]);
     return (
         <div className={`${styles.overViewDetailClass} ${styles.flex1} overViewDetaiClass`}>
             <div className={`${styles.colLeft} col`}>
