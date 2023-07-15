37b872e6f55f7bc35f0727b2f80310d000210ea1
NVC <nguyencuong21520@gmail.com>
2023-07-16 01:23:15 +0700

    location made by NVC

---
+++ src/components/Location/CreateLocation/index.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import { Button, Input } from 'antd';
+
+import styles from '@/styles/class/CreateLocation.module.scss';
+import Dropdown from '@/components/Dropdown';
+
+interface Props {
+    onReceive?: (status: boolean) => void;
+}
+
+
+const CreateLocation = (props: Props) => {
+    const [area, setArea] = useState<string>('');
+    const locationList = [
+        {
+            key: "HN",
+            label: "Hà Nội"
+        },
+        {
+            key: "HCM",
+            label: "Hồ Chí Minh"
+        },
+        {
+            key: "ĐN",
+            label: "Đà Nẵng"
+        },
+    ]
+    return (
+        <div className={styles.containerCreateClass}>
+            <Form>
+                <Form.Group className={styles.mb_15}>
+                    <Form.Label>Tên cơ sở</Form.Label>
+                    <Input type="text" name="codeClass" placeholder="Tên cơ sở" size="middle" className={styles.input} />
+                </Form.Group>
+                <Form.Group className={styles.mb_15}>
+                    <Form.Label>Mã cơ sở:</Form.Label>
+                    <Input type="text" name="codeClass" placeholder="Mã cơ sở" size="middle" className={styles.input} />
+                </Form.Group>
+                <Form.Group className={styles.mb_15}>
+                    <Form.Label>Địa chỉ cơ sở</Form.Label>
+                    <Input type="text" name="codeClass" placeholder="Địa chỉ cơ sở" size="middle" className={styles.input} />
+                </Form.Group>
+                <Form.Group className={"w-100"}>
+                    <Form.Label>Khu Vực</Form.Label>
+                    <Dropdown
+                        className={styles.weekday}
+                        trigger='click'
+                        listSelect={locationList}
+                        keyIndex='weekdayTwice'
+                        title={area ? area : 'Chọn khu vực'}
+                        onClickItem={(e) => {
+                            const label = locationList.find((item)=>item.key === e.key);
+                            setArea(label?.label as string);
+                        }}
+                    />
+                </Form.Group>
+                <Button  htmlType='submit' className={styles.fl_r}>Tạo lớp</Button>
+
+            </Form>
+        </div >
+    )
+}
+
+export default CreateLocation;
--- src/components/Location/index.tsx
+++ src/components/Location/index.tsx
@@ -1,9 +1,134 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Tabs from '../Tabs';
+import { TabsProps } from 'antd';
+import ToolBar from '../Tabs/ToolBar';
+import ModalCustomize from '../ModalCustomize';
+import Table from '../Table';
+import CreateLocation from './CreateLocation';
+
+import styles from '@/styles/class/Class.module.scss';
+import { Columns, RowData } from '@/global/interface';
+
+
+
+const area: TabsProps['items'] = [
+    {
+        key: 'ALL_Location',
+        label: 'Tất cả',
+    },
+    {
+        key: 'HN',
+        label: `Hà Nội`,
+    },
+    {
+        key: 'HCM',
+        label: `Hồ Chí Minh`,
+    },
+    {
+        key: 'ĐN',
+        label: `Đà Nẵng`,
+    },
+]
+const columns: Columns = [
+    {
+        key: 'locationCode',
+        dataIndex: 'locationCode',
+        title: 'Mã cơ sở',
+
+    },
+    {
+        key: 'locationName',
+        dataIndex: 'locationName',
+        title: 'Tên cơ sở',
+
+    },
+    {
+        key: 'locationDetail',
+        dataIndex: 'locationDetail',
+        title: 'Địa chỉ',
+    },
+    {
+        key: 'locationArea',
+        dataIndex: 'locationArea',
+        title: 'Khu Vực',
+
+    }
+];
+
+const dataSource: RowData[] = [
+    {
+        key: 'eeeee',
+        locationCode: 'HĐT',
+        locationName: 'Hoàng Đạo Thuý',
+        locationDetail: 'Tầng 4, Tòa B3.7 Hacinco, Hoàng Đạo Thúy, Quận Thanh Xuân, Hà Nội',
+        locationArea: "Hà Nội"
+    },
+    {
+        key: 'aaaa',
+        locationCode: 'TC',
+        locationName: 'Thành Công',
+        locationDetail: 'Tầng 6, Tòa nhà Chigamex 22C Thành Công, Phường Thành Công, Quận Ba Đình, Hà Nội',
+        locationArea: "Hà Nội"
+    },
+];
+
 
 const Location = () => {
+
+    const [storeManagerLocation, setStoreManagerLocation] = useState<{
+        crrKeyTab: string;
+        listFieldFilter: Array<{
+            key: string;
+            value: any;
+        }>
+    }>({
+        crrKeyTab: area[0].key,
+        listFieldFilter: [],
+    });
+
+    const [openModal, setOpenModal] = useState<boolean>(false);
+
     return (
-        <div>Location</div>
+        <div >
+            <Tabs listItemTab={area} activeKey={storeManagerLocation.crrKeyTab} onClickTab={(key) => {
+                setStoreManagerLocation({
+                    ...storeManagerLocation,
+                    crrKeyTab: key
+                })
+            }} />
+            <ToolBar
+                listFilter={[]}
+                createButton
+                exportCSVButton
+                onClickCreateButton={() => {
+                    setOpenModal(true);
+                }}
+                iconReload
+            />
+            <Table
+                className={styles.tableMangerClass}
+                columns={columns}
+                rowData={dataSource}
+                enableRowSelection
+                disableDefaultPagination
+        
+            />
+
+                  {
+                openModal && <ModalCustomize
+                    show={openModal}
+                    modalHeader={<h2>Thêm cơ sở</h2>}
+                    onHide={() => {
+                        setOpenModal(false);
+                    }}
+                    size='lg'
+                >
+                    <CreateLocation />
+                </ModalCustomize>
+            }
+        </div>
     )
 }
 
 export default Location;
+
