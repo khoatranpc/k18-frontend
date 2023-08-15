13492e0873d25fa848b436fb8ff0168bf24a09b7
Tran Khoa <khoatranpc603@gmail.com>
2023-08-15 14:35:09 +0700

    add env

---
+++ src/store/reducers/class/classTeacherPoint.reducer.ts
@@ -0,0 +1,4 @@
+import { createSliceReducer } from "@/utils/redux-toolkit";
+
+const classTeacherPoint = createSliceReducer('classTeacherPoint');
+export default classTeacherPoint.reducer;
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Obj } from "@/global/interface";
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: process.env.NEXT_PUBLIC_SERVER_API,
 });
 httpClient.interceptors.request.use(function (config) {
     (config.headers as Obj).Authorization = localStorage.getItem('access_token') as string;
