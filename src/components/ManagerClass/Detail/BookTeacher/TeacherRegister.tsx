467d20c50b110859dc314c2c806695db8b50d953
Tran Khoa <khoatranpc603@gmail.com>
2023-11-24 18:33:05 +0700

    refactor

--- src/components/ManagerClass/Detail/BookTeacher/TeacherRegister.tsx
+++ src/components/ManagerClass/Detail/BookTeacher/TeacherRegister.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Radio } from 'antd';
 import { ROLE_TEACHER } from '@/global/enum';
-import styles from '@/styles/class/BookTeacher.module.scss';
+import { Obj } from '@/global/interface';
 import { useHandleTeacherInRCBT } from '@/utils/hooks';
 import useGetCrrUser from '@/utils/hooks/getUser';
-import { Obj } from '@/global/interface';
+import { useHookMessage } from '@/utils/hooks/message';
+import styles from '@/styles/class/BookTeacher.module.scss';
 
 interface Props {
     isRegister?: boolean;
@@ -15,24 +16,31 @@ const TeacherRegister = (props: Props) => {
     const [role, setRole] = useState<ROLE_TEACHER>(ROLE_TEACHER.MT);
     const currentUser = useGetCrrUser()?.data as Obj;
     const register = useHandleTeacherInRCBT();
+    const message = useHookMessage();
     const handleSubmit = () => {
-        if (props.isRegister) {
-            register.query({
-                payload: {
+        register.query({
+            payload: {
+                query: {
                     query: {
-                        query: {
-                            options: 'ADD',
-                            role: role,
-                            idTeacher: currentUser?._id
-                        },
-                        params: [props.recordBookTeacherId as string]
-                    }
+                        options: props.isRegister ? 'REGISTER' : 'CANCEL',
+                        role: role,
+                        idTeacher: currentUser?._id
+                    },
+                    params: [props.recordBookTeacherId as string]
                 }
-            })
-        } else if (props.isCancel) {
-
-        }
+            }
+        });
     }
+    useEffect(() => {
+        if (register.dataHandle.response) {
+            message.open({
+                content: register.dataHandle.response.message as string,
+                type: register.dataHandle.success ? 'success' : 'error'
+            });
+            register.clear();
+            message.close();
+        }
+    }, [register.dataHandle.response]);
     return (
         <div className={styles.registerClass}>
             <p>{props.isRegister ? 'Bạn muốn giảng dạy với vị trí nào?' : 'Xác nhận huỷ đăng ký lớp!'}</p>
@@ -44,7 +52,7 @@ const TeacherRegister = (props: Props) => {
                 <Radio value={ROLE_TEACHER.SP} /> <span className={styles.label}>SP</span>
             </Radio.Group>
             }
-            <Button onClick={handleSubmit}>Đăng ký</Button>
+            <Button onClick={handleSubmit} loading={register.dataHandle.isLoading}>{props.isRegister ? 'Đăng ký' : 'Xác nhận huỷ'}</Button>
         </div>
     )
 }
