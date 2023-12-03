6b1ff9b8dc4dc20bf5423bea58c8a933529a3547
Tran Khoa <khoatranpc603@gmail.com>
2023-12-03 14:05:38 +0700

    init manager list te

--- src/layouts/containerPage/tab.tsx
+++ src/layouts/containerPage/tab.tsx
@@ -349,6 +349,17 @@ const tabForRole: Record<ROLE_USER, Array<TabRoute>> = {
       noReplaceTitle: true,
       positionTE: PositionTe.LEADER
     },
+    // {
+    //   title: "Danh sách TE",
+    //   route: CombineRoute["TE"]["MANAGER"]["STAFF"],
+    //   key: "STAFF",
+    //   showIcon: true,
+    //   keyIcon: KEY_ICON.EMPLOYEE,
+    //   indexRoute: CombineRoute["TE"]["MANAGER"]["STAFF"],
+    //   component: ComponentPage.TE_STAFF,
+    //   noReplaceTitle: true,
+    //   positionTE: PositionTe.LEADER
+    // },
   ],
   TEACHER: [
     {
