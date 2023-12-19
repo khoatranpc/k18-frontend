bc0a8c8e7dea3c014a53a1d0fe4f191c2c1ae8a0
Tran Khoa <khoatranpc603@gmail.com>
2023-12-19 18:01:44 +0700

    init for manager te

--- src/pages/te/manager/staff/index.tsx
+++ src/pages/te/manager/staff/index.tsx
@@ -2,5 +2,5 @@ import { ROLE_USER } from "@/global/enum";
 import ContainerPage from "@/layouts/containerPage/containerPage";
 import CreatePage from "@/utils/hocs/ProviderPage";
 
-const Staff = CreatePage("", ROLE_USER.TE, ContainerPage);
+const Staff = CreatePage("TEs", ROLE_USER.TE, ContainerPage);
 export default Staff;
