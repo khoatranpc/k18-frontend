ef19dbfc35b66147a9f170c91f2405cf5938f313
Tran Khoa <khoatranpc603@gmail.com>
2024-01-09 15:24:36 +0700

    update handle breadcumb folder, file/

--- src/components/NewDocument/config.ts
+++ src/components/NewDocument/config.ts
@@ -15,7 +15,6 @@ const findNearestParent = (tree: Obj[], key: string, parent: Obj | null = null):
     }
     return null;
 };
-
 const listToTreeFnc = (listData: Obj[]) => {
     const newList: Obj[] = [...listData];
     for (let i = 0; i < newList.length; i++) {
@@ -34,8 +33,32 @@ const listToTreeFnc = (listData: Obj[]) => {
     });
     return tree
 }
+const findRootNodeByPath = (listData: Obj[], childNodeKey: string): Obj | null | undefined => {
+    const findNode = listData.find((item) => item._id === childNodeKey);
+    if (findNode) {
+        const findRootNode = listData.find((item) => {
+            return String(findNode.path).includes(item.path) && String(item.path).split('/').length === 1;
+        });
+        return findRootNode as Obj;
+    }
+    return null;
+}
+
 const findCurrentNode = (nodeKey: string, listData: Obj[]) => {
-    const crrNode = listData.findIndex(item => item.key === nodeKey);
+    const crrNode = listData.findIndex(item => item.key === nodeKey || item._id === nodeKey);
     return listData[crrNode];
 }
-export { findNearestParent, findCurrentNode, listToTreeFnc };
+
+const mapNodeParentByPath = (nodeKey: string, listData: Obj[]): Obj[] => {
+    const currentNode = findCurrentNode(nodeKey, listData);
+    if (currentNode) {
+        const listParent = String(currentNode.path).split('/')
+        const getListParent = listParent.map((item) => {
+            const dataParent = listData.find((data) => data._id === item);
+            return dataParent;
+        }).filter((item) => item);
+        return getListParent as Obj[] || [];
+    }
+    return [];
+}
+export { findNearestParent, findCurrentNode, listToTreeFnc, findRootNodeByPath, mapNodeParentByPath };
