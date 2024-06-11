c9ec3fe892460af13500dacf53b7a13484cc24fc
khotranpc <khotranpc603@gmail.com>
2024-06-11 10:18:55 +0700

    update logic filter feedback response

--- src/components/ManageFeedback/index.tsx
+++ src/components/ManageFeedback/index.tsx
@@ -3,25 +3,24 @@ import { TabsProps } from 'antd';
 import Tabs from '../Tabs';
 import styles from '@/styles/feedback/Feedback.module.scss';
 import ListClass from './ListClass';
-import Question from './Question';
 import CollectionAnswer from './CollectionAnswer';
 
 export enum ListTabFeedBack {
-    QUESTION = 'QUESTION',
+    // QUESTION = 'QUESTION',
     COLLECTION_ANSWER = 'COLLECTION_ANSWER',
     STATUS_CLASS_ENABLED = 'STATUS_CLASS_ENABLED'
 }
 export const getLabelListTabFeedBack: Record<ListTabFeedBack, string> = {
     COLLECTION_ANSWER: 'Câu trả lời',
-    QUESTION: ' Câu hỏi',
+    // QUESTION: ' Câu hỏi',
     STATUS_CLASS_ENABLED: 'Tổng hợp lớp'
 }
 const FormFeedback = () => {
     const listItemTab: TabsProps['items'] = [
-        {
-            key: ListTabFeedBack.QUESTION,
-            label: getLabelListTabFeedBack[ListTabFeedBack.QUESTION]
-        },
+        // {
+        //     key: ListTabFeedBack.QUESTION,
+        //     label: getLabelListTabFeedBack[ListTabFeedBack.QUESTION]
+        // },
         {
             key: ListTabFeedBack.COLLECTION_ANSWER,
             label: getLabelListTabFeedBack[ListTabFeedBack.COLLECTION_ANSWER]
@@ -33,10 +32,10 @@ const FormFeedback = () => {
     ];
     const getComponentTab: Record<ListTabFeedBack, React.ReactElement> = {
         STATUS_CLASS_ENABLED: <ListClass />,
-        QUESTION: <Question />,
+        // QUESTION: <Question />,
         COLLECTION_ANSWER: <CollectionAnswer />
     }
-    const [currentTab, setCurrentTab] = useState<ListTabFeedBack>(ListTabFeedBack.QUESTION);
+    const [currentTab, setCurrentTab] = useState<ListTabFeedBack>(ListTabFeedBack.COLLECTION_ANSWER);
     return (
         <div className={styles.containerFormfeedback}>
             <Tabs
