a4e66b4dd83638904409d7811ed9503515410ed1
NVC_macbook16 <nguyencuong21520@gmail.com>
2024-07-03 02:03:52 +0700

    feat: add ui create teacher

--- src/components/ManageTeacher/index.tsx
+++ src/components/ManageTeacher/index.tsx
@@ -1,47 +1,49 @@
-import React, { useState } from 'react';
-import { TabsProps } from 'antd';
-import Tabs from '../Tabs';
-import ListTeacher from './ListTeacher';
-import styles from '@/styles/teacher/ManagerTeacher.module.scss';
+import React, { useState } from "react";
+import { TabsProps } from "antd";
+import Tabs from "../Tabs";
+import ListTeacher from "./ListTeacher";
+import styles from "@/styles/teacher/ManagerTeacher.module.scss";
 
 export enum TabManagerTeacher {
-    // STATISTIC = 'STATISTIC',
-    LISTTEACHER = 'LISTTEACHER',
-    // WAITINGACCEPT = 'WAITINGACCEPT'
+  // STATISTIC = 'STATISTIC',
+  LISTTEACHER = "LISTTEACHER",
+  // WAITINGACCEPT = 'WAITINGACCEPT'
 }
 export const getLableTabManagerTeacher: Record<TabManagerTeacher, string> = {
-    LISTTEACHER: 'Danh sách',
-    // STATISTIC: 'Thống kê',
-    // WAITINGACCEPT: 'Đợi duyệt'
-}
-const listTab: TabsProps['items'] = [
-    {
-        key: TabManagerTeacher.LISTTEACHER,
-        label: getLableTabManagerTeacher[TabManagerTeacher.LISTTEACHER]
-    },
+  LISTTEACHER: "Danh sách",
+  // STATISTIC: 'Thống kê',
+  // WAITINGACCEPT: 'Đợi duyệt'
+};
+const listTab: TabsProps["items"] = [
+  {
+    key: TabManagerTeacher.LISTTEACHER,
+    label: getLableTabManagerTeacher[TabManagerTeacher.LISTTEACHER],
+  },
 ];
 
 const getComponentTab: Record<TabManagerTeacher, React.ReactElement> = {
-    // STATISTIC: <Statistic />,
-    LISTTEACHER: <ListTeacher />,
-}
+  // STATISTIC: <Statistic />,
+  LISTTEACHER: <ListTeacher />,
+};
 
 const ManagerTeacher = () => {
-    const [tab, setTab] = useState<TabManagerTeacher>(TabManagerTeacher.LISTTEACHER);
+  const [tab, setTab] = useState<TabManagerTeacher>(
+    TabManagerTeacher.LISTTEACHER
+  );
 
-    return (
-        <div className={styles.managerTeacher}>
-            <Tabs
-                activeKey={tab}
-                listItemTab={listTab}
-                notAllowContent
-                onClickTab={(key) => {
-                    setTab(key as TabManagerTeacher);
-                }}
-            />
-            {getComponentTab[tab]}
-        </div>
-    )
-}
+  return (
+    <div className={styles.managerTeacher}>
+      <Tabs
+        activeKey={tab}
+        listItemTab={listTab}
+        notAllowContent
+        onClickTab={(key) => {
+          setTab(key as TabManagerTeacher);
+        }}
+      />
+      {getComponentTab[tab]}
+    </div>
+  );
+};
 
 export default ManagerTeacher;
