a849b3d9980b4afcdb64b0dc064dfd385ca2c6c5
khotranpc <khotranpc603@gmail.com>
2024-07-05 09:54:01 +0700

    hot fix texteditor

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -55,7 +55,7 @@ const TextEditor = (props: Props) => {
     );
 }
 const MemoTextEditor = memo(TextEditor, (prevProps, nextProps) => {
-    if ((prevProps.title !== nextProps.title) || (prevProps.loadingButton !== nextProps.loadingButton)) return false;
+    if ((prevProps.title !== nextProps.title) || (prevProps.loadingButton !== nextProps.loadingButton) || (prevProps.disabledSave !== nextProps.disabledSave)) return false;
     return true;
 });
 export default MemoTextEditor;
