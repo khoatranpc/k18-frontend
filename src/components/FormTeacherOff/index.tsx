b75c89a7a2b94c2cdf5f5693461f8d333ed2c2c3
khotranpc <khotranpc603@gmail.com>
2024-08-12 00:56:22 +0700

    feat(interview): update set interview

--- src/components/FormTeacherOff/index.tsx
+++ src/components/FormTeacherOff/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const FormTeacherOff = () => {
     return (
-        <div className="formTeacherOff w-[50vw] bg-[red]">
-            FormTeacherOff
+        <div className="formTeacherOff min-h-[100vh]">
+            <div className="content w-[50vw] m-auto p-[2.4rem]">
+                <div className="title">
+                    <h1 className="text-center text-[3.6rem]">Mindx School - Yêu cầu nghỉ phép</h1>
+                    <p>Form thông tin yêu cầu nghỉ phép chỉ dành cho</p>
+                </div>
+            </div>
         </div>
     )
 }
