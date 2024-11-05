b4dc37109bcb30766ab576ad9295b7916ffb68b0
khoatranpc <khoatranpc603@gmail.com>
2024-11-05 10:06:24 +0700

    update api lark

--- src/store/reducers/lark/sendLark.reducer.ts
+++ src/store/reducers/lark/sendLark.reducer.ts
@@ -1,7 +1,7 @@
 import { METHOD } from "@/global/enum";
 import { createRequest, createSliceReducer } from "@/utils/redux-toolkit";
 
-export const querySendLark = createRequest('QUERY_SEND_LARK', '/lark', METHOD.POST);
+export const querySendLark = createRequest('QUERY_SEND_LARK', '/api/v1/lark', METHOD.POST);
 const sendLark = createSliceReducer('sendLark', querySendLark);
 
 export default sendLark.reducer;
